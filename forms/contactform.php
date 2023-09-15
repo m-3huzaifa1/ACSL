54630d953d46d58d54b8f19ef764d8b14320794a
Mohd Huzaifa <112268278+m-3huzaifa1@users.noreply.github.com>
2023-09-15 23:44:24 +0530

    Update contactform.php

--- forms/contactform.php
+++ forms/contactform.php
@@ -4,23 +4,35 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $subject = $_POST["subject"];
     $message = $_POST["message"];
-
+    
+    error_log("Name: " . $name);
+    error_log("Email: " . $email);
+    error_log("Subject: " . $subject);
+    error_log("Message: " . $message);
+    
     $to = "mohdhuzaifa80@gmail.com"; // Replace with your email address
     $headers = "From: $email\r\n";
     $headers .= "Reply-To: $email\r\n";
     $headers .= "Content-Type: text/html\r\n";
-
+    
+    error_log("headers: " . $headers);
+    
     $mailBody = "Name: $name<br>";
     $mailBody .= "Email: $email<br>";
     $mailBody .= "Subject: $subject<br>";
     $mailBody .= "Message:<br>$message";
-
+    
+    error_log("body: " . $mailbody);
+    
     if (mail($to, $subject, $mailBody, $headers)) {
         echo "success";
+        error_log("Success");
     } else {
         echo "error";
+        error_log("Error");
     }
 } else {
     echo "Invalid request";
+    error_log("Invalid request");
 }
 ?>
